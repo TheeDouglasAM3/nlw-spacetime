0adbf2541ec9f86e7a32327e28c8b6f4d12d4dc0
Douglas Alves Marcelino <douglas.alves@havan.com.br>
2023-05-17 16:05:20 -0300

    config: finish commit backend structure

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,18 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.listen({
-  port: 3333
-}).then(() => {
-	console.log('🤖 HTTP server running on http://localhost:3333')
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+  return users
 })
 
-//30:32 - video 1
-//https://youtu.be/FNYRTmXBAVY
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🤖 HTTP server running on http://localhost:3333')
+  })
